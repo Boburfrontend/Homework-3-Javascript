ac9d7601857ea03adf92b5dc75c944f183de6c7e
Boburfrontend <example@gmail.com>
2023-06-03 20:48:35 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,279 @@
+//  boolean
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let isAGreater = a>0 ;
+//console.log(isAGreater);
+//console.log(typeof isAGreater,typeof a)
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let isAGreater=a>a/3;
+//console.log(isAGreater);
+//console.log(typeof isAGreater,typeof a)
+
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let isAGreater = a>a/2 ;
+//console.log(isAGreater);
+//console.log(typeof isAGreater,typeof a)
+
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let b=+prompt("b ni kiriting")
+//let S=a>2 ;
+//let D=b<=3;
+//console.log(S,D);
+//console.log(typeof S,typeof D)
+
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let b=+prompt("b ni kiriting")
+//let E=a>=0 ;
+//let G=b<-2;
+//console.log(E,G);
+//console.log(typeof E,typeof G)
+
+
+//true & false
+//let a =+prompt("a ni kiriting")
+//let b=+prompt("b ni kiriting")
+//let c=+prompt("c ni kiriting")
+//let S=c=0;
+//let G=b<=c;
+//let D=a<=b;
+//console.log(S,G,D);
+//console.log(typeof S,typeof G,typeof D)
+
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let b=+prompt("b ni kiriting")
+//let c=+prompt("c ni kiritnig")
+//let S=a<b ;
+//let D=b<c;
+//let C=a<b;
+//console.log(S,D,C);
+//console.log(typeof S,typeof D, typeof C)
+
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let b=+prompt("b ni kiriting")
+//let S=a>a/3 ;
+//let D=b>b/3;
+//console.log(S,D);
+//console.log(typeof S,typeof D)
+
+
+//true & false
+//let a =+prompt("a ni kiritiing")
+//let b=+prompt("b ni kiriting")
+//let F=a>a/2;
+//let H=b<b/2;
+//console.log(F,H);
+//console.log(typeof F,typeof H )
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a/2;
+//let D = b < b/3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//  boolean
+//true & false
+//let a = -16;
+//let b = 18;
+//let c = -1;
+//let isAGreater = a > 0 | b > 0 | c > 0;
+//console.log(isAGreater)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 2;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a = a/2;
+//let D = b > b / 3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 2;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 2;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 3;
+//let D = b > b / 2;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 2;
+//let D = b > b / 2;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+//let a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a / 2;
+//let D = b > b / 3;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+//true & false
+// a = +prompt("a ni kiritiing")
+//let b = +prompt("b ni kiriting")
+//let S = a > a /2;
+//let D = b > b / 2;
+//console.log(S, D);
+//console.log(typeof S, typeof D,)
+
+
+// operators //
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let Y=-B/2*(A**2+B**2);
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let D=+prompt("D ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let Y=(A+D/C+D)**2+B**2;
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let D=+prompt("D ni kiriting")
+//let Y=2*(A**D+4*C**2/3);
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let D=+prompt("D ni kiriting")
+//let Y=-1/A**2*(B/10)**3*(C+D)**2;
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let D=+prompt("D ni kiriting")
+//let Y=(A/B-1/C/D-1)**2;
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let D=+prompt("D ni kiriting")
+//let Y=A+B/C+D/A+C;
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let Y=Math.sqrt(A+B+C)**2-(A-B-C)**2;
+//console.log(Y)
+
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let Y=A**2+B**2+C**2/(A*B*C);
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let D=+prompt("D ni kiriting")
+//let Y=A*D**3/3+B*C**2/2;
+//console.log(Y)
+
+
+//let A=+prompt("A ni kiriting")
+//let B=+prompt("B ni kiriting")
+//let C=+prompt("C ni kiriting")
+//let Y=Math.absA/B-B/C+4*Math.sqrtA+C;
+//console.log(Y)
